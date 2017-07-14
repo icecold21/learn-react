acbfd5033be1fc705e56f45a4b3e6bc1ccf3b53a
Jonathan <jonathan.wylliem@jurnal.id>
2017-07-15 02:16:10 +0700

    Lecture 20 - Calculating time until

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -14,7 +14,20 @@ class Clock extends Component {
 
     console.log('this.props', this.props);
   }
+
+  getTimeUntil(deadline) {
+    const time    = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor(time / 1000) % 60;
+    const minutes = Math.floor(time / 1000 / 60) % 60;
+    const hours   = Math.floor(time / 1000 / 3600) % 24;
+    const days    = Math.floor(time / 1000 / 3600 / 24);
+
+    console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
+    this.setState({days: days});
+  }
+
   render() {
+    this.getTimeUntil(this.props.deadline);
     return(
       <div>
         <div className='Clock-days'>{ this.state.days } days</div>
